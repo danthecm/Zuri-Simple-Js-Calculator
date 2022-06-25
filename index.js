c717274d9f9ec3e7cb0ce0aa63cee3c4467f54e5
CM <etchiedaniel1@gmail.com>
2022-06-25 07:52:42 +0100

    fixed small issues

--- index.js
+++ index.js
@@ -4,29 +4,28 @@ const simpleCalculator = () => {
     "What would you like to do: \n(Add, Substract, Divide, Multiply)"
   ).toLocaleLowerCase();
 
-    if (validOperations.includes(operation)) {
-      num1 = +prompt("Enter the first number");
-      num2 = +prompt("Enter second number");
-      let result;
+  if (validOperations.includes(operation)) {
+    num1 = +prompt("Enter the first number");
+    num2 = +prompt("Enter second number");
+    let result;
 
-      switch (operation) {
-        case "add":
-            result = num1 + num2
-            break;
-        case "substract":
-            result = num1 - num2
-        case "divide":
-            result = num1 /num2
-        default:
-            result = 0;
-      }
-
-      alert(`The result of the ${operation} operation is: ${result}`)
-      return
-
-    } else {
-        alert("Invalid operation entered")
-        simpleCalculator()
+    switch (operation) {
+      case "add":
+        result = num1 + num2;
+        break;
+      case "substract":
+        result = num1 - num2;
+      case "divide":
+        result = num1 / num2;
+      default:
+        result = 0;
+    }
+    
+    alert(`The result of the ${operation} operation is: ${result}`);
+    return;
+  } else {
+    alert("Invalid operation entered");
+    simpleCalculator();
   }
 };
 
